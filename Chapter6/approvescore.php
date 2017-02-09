2ab3b0858848cbd3c06276759d9a660021eba093
Bobby P <abpupeerapitug@yahoo.com>
2017-02-08 21:37:47 -0600

    Added the form confirmation for approving score.

--- Chapter6/approvescore.php
+++ Chapter6/approvescore.php
@@ -22,9 +22,24 @@ require_once('authorize.php');
             require_once('appvars.php');
             require_once('connectvars.php');
 
-            $score = $_GET['score'];
-            $id = $_GET['id'];
-            $name = $_GET['name'];
+            if(isset($_GET['id']) && isset($_GET['date']) && isset($_GET['name']) && isset($_GET['score'])) {
+                // Grab the score data from the GET
+                $id = $_GET['id'];
+                $date = $_GET['date'];
+                $name = $_GET['name'];
+                $score = $_GET['score'];
+                $screenshot = $_GET['screenshot'];
+            }
+
+            else if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['score'])) {
+                // Grab the score data from the POST
+                $id = $_POST['id'];
+                $name = $_POST['name'];
+                $score = $_POST['score'];
+            }
+            else {
+                echo '<p class="error">Sorry, no high score was specified for approval.</p>';
+            }
 
             if(isset($_POST['submit'])) {
                 if($_POST['confirm'] == 'Yes') {
@@ -43,6 +58,21 @@ require_once('authorize.php');
                 else {
                     echo '<p class="error">Sorry, there was a problem approving the high score.</p>';
                 }
+            }
+
+            else if (isset($id) && isset($name) && isset($date) && isset($score)) {
+                echo '<p>Are you sure you want to approve the following high score?</p>';
+                echo '<p><strong>Name: </strong>' . $name . '<br /><strong>Date: </strong>' . $date .
+                    '<br /><strong>Score: </strong>' . $score . '</p>';
+                echo '<form method="post" action="approvescore.php">';
+                echo '<img src="' . GW_UPLOADPATH . $screenshot . '" width="160" alt="Score image" /><br />';
+                echo '<input type="radio" name="confirm" value="Yes" /> Yes ';
+                echo '<input type="radio" name="confirm" value="No" checked="checked" /> No <br />';
+                echo '<input type="submit" value="Submit" name="submit" />';
+                echo '<input type="hidden" name="id" value="' . $id . '" />';
+                echo '<input type="hidden" name="name" value="' . $name . '" />';
+                echo '<input type="hidden" name="score" value="' . $score . '" />';
+                echo '</form>';
             }
 
             echo '<p><a href="admin.php">&lt;&lt; Back to admin page</a></p>';
