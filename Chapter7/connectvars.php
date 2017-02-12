cb4cdf4bb3398ffe27b41ae8f0b40f9ea06beb37
Bobby P <abpupeerapitug@yahoo.com>
2017-02-12 01:45:45 -0600

    Modified connectvars.php

--- Chapter7/connectvars.php
+++ Chapter7/connectvars.php
@@ -1,6 +1,6 @@
 <?php
   // Define database connection constants
-  define('DB_HOST', 'data.mis-match.net');
+  define('DB_HOST', 'localhost');
   define('DB_USER', 'ABPxNiNjA');
   define('DB_PASSWORD', 'test');
   define('DB_NAME', 'mismatchdb');
