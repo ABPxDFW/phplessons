6905c8785232c27995d01900c9215629b1a6f94a
Bobby P <abpupeerapitug@yahoo.com>
2017-02-17 23:58:51 -0600

    Fixed the session section

--- Chapter7/index.php
+++ Chapter7/index.php
@@ -1,3 +1,17 @@
+<?php
+
+    session_start();
+
+    // If the session vars aren't set, try to set them with a cookie
+    if (!isset($_SESSION['user_id'])) {
+        if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
+            $_SESSION['user_id'] = $_COOKIE['user_id'];
+            $_SESSION['username'] = $_COOKIE['username'];
+        }
+    }
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -10,8 +24,6 @@
         <h3>Mismatch - Where opposites attract!</h3>
 
     <?php
-
-        session_start();
 
         require_once('appvars.php');
         require_once('connectvars.php');
