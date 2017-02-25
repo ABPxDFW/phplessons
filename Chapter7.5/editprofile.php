eb7fa278751bf03dc461c5f03634acb33205110e
Bobby P <abpupeerapitug@yahoo.com>
2017-02-24 22:12:25 -0600

    Adding navmenu.php and modifying editprofile.php

--- Chapter7.5/editprofile.php
+++ Chapter7.5/editprofile.php
@@ -1,13 +1,8 @@
 <?php
-session_start();
+require_once('startsession.php');
 
-// If the session vars aren't set, try to set them with a cookie
-if (!isset($_SESSION['user_id'])) {
-    if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
-        $_SESSION['user_id'] = $_COOKIE['user_id'];
-        $_SESSION['username'] = $_COOKIE['username'];
-    }
-}
+$page_title = 'Where opposites attract!';
+require_once('header.php');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
---
+++ Chapter7.5/navmenu.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 2/24/2017
+ * Time: 9:56 PM
+ */
