31657cad91320afba7bb998aab79ac9e3a8d3a81
Bobby P <abpupeerapitug@yahoo.com>
2017-03-27 23:30:18 -0500

    Modified search.php to include sort function for results.

--- Chapter9/search.php
+++ Chapter9/search.php
@@ -13,16 +13,18 @@
 
 <?php
     // Grab the sort setting and search keywords from the URL using GET
-    //$sort = $_GET['sort'];
+    //$sort = "";
+
+    isset($_GET['sort']) ? $sort = $_GET['sort'] : $sort = '';
     $user_search = $_GET['usersearch'];
 
     // Start generating the table of results
     echo '<table border="0" cellpadding="2">';
 
     // Generate the search result headings
-    echo '<tr class="heading">';
-    echo '<td>Job Title</td><td>Description</td><td>State</td><td>Date Posted</td>';
-    echo '</tr>';
+ //   echo '<tr class="heading">';
+ //   echo '<td>Job Title</td><td>Description</td><td>State</td><td>Date Posted</td>';
+ //   echo '</tr>';
 
     // Connect to the database
     require_once('connectvars.php');
@@ -30,7 +32,7 @@
 
     // Query to get the results
 
-    function build_query ($user_search)
+    function build_query ($user_search, $sort)
     {
         $search_query = "SELECT * FROM riskyjobs";
         $user_search = $_GET['usersearch'];
@@ -63,10 +65,98 @@
             $search_query .= " WHERE $where_clause";
         }
 
+        // Sort the search query using the sort setting
+        switch($sort) {
+
+            // Ascending by job title
+            case 1:
+                $search_query .= " ORDER BY title";
+                break;
+
+            // Descending by job title
+            case 2:
+                $search_query .= " ORDER BY title DESC";
+                break;
+
+            // Ascending by state
+            case 3:
+                $search_query .= " ORDER BY state";
+                break;
+
+            // Descending by state
+            case 4:
+                $search_query .= " ORDER BY state DESC";
+                break;
+
+            // Ascending by date posted (oldest first)
+            case 5:
+                $search_query .= " ORDER BY date_posted";
+                break;
+
+            // Descending by date posted (newest first)
+            case 6:
+                $search_query .= " ORDER BY date_posted DESC";
+                break;
+
+            default:
+
+                // No sort setting provided, so don't sort the query
+        }
+
         return $search_query;
     }
 
-    $search_query = build_query($user_search);
+    function generate_sort_links ($user_search, $sort) {
+        $sort_links = '';
+
+        switch ($sort) {
+            case 1:
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=2">Job Title</a></td><td>Description</td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">State</a></td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=5">Date Posted</a></td>';
+
+                break;
+
+            case 3:
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=4">State</a></td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=5">Date Posted</a></td>';
+
+                break;
+
+            case 5:
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">State</a></td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=6">Date Posted</a></td>';
+
+                break;
+
+            default:
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=1">Job Title</a></td><td>Description</td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=3">State</a></td>';
+
+                $sort_links .= '<td><a href="' . $_SERVER['PHP_SELF'] . '?usersearch=' . $user_search . '&sort=5">Date Posted</a></td>';
+
+        }
+        return $sort_links;
+    }
+
+    // Start generating the table of results
+    echo '<table border="0" cellpadding="2">';
+
+    // Generate the search result headings
+    echo '<tr class="heading">';
+    echo generate_sort_links($user_search, $sort);
+    echo '</tr>';
+
+    $search_query = build_query($user_search, $sort);
 
     //echo $search_query;
     $result = mysqli_query($dbc, $search_query) or die ("Unable to execute query");
