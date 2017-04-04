164f3ae3edee45fea3a0424022b38b72dc5d2208
Bobby P <abpupeerapitug@yahoo.com>
2017-04-03 22:17:09 -0500

    Modified the phone field to use regex.

--- Chapter10/registration.php
+++ Chapter10/registration.php
@@ -39,9 +39,11 @@
       $output_form = 'yes';
     }
 
-    if (empty($phone)) {
+//    if (empty($phone)) { old code
       // $phone is blank
-      echo '<p class="error">You forgot to enter your phone number.</p>';
+
+      if (!preg_match('/^\(?[2-9]\d{2}\)?[-\s]\d{3}[-\s]\d{4}$/', $phone)){
+      echo '<p class="error">Your phone number is invalid.</p>';
       $output_form = 'yes';
     }
 
@@ -69,24 +71,24 @@
   <table>
     <tr>
       <td><label for="firstname">First Name:</label></td>
-      <td><input id="firstname" name="firstname" type="text" value="<?php echo $first_name; ?>"/></td></tr>
+      <td><input id="firstname" name="firstname" type="text" value="<?php echo isset($first_name) ? $first_name :''; ?>"/></td></tr>
     <tr>
       <td><label for="lastname">Last Name:</label></td>
-      <td><input id="lastname" name="lastname" type="text" value="<?php echo $last_name; ?>"/></td></tr>
+      <td><input id="lastname" name="lastname" type="text" value="<?php echo isset($last_name) ? $last_name : ''; ?>"/></td></tr>
     <tr>
       <td><label for="email">Email:</label></td>
-      <td><input id="email" name="email" type="text" value="<?php echo $email; ?>"/></td></tr>
+      <td><input id="email" name="email" type="text" value="<?php echo isset($email) ? $email : ''; ?>"/></td></tr>
     <tr>
       <td><label for="phone">Phone:</label></td>
-      <td><input id="phone" name="phone" type="text" value="<?php echo $phone; ?>"/></td></tr>
+      <td><input id="phone" name="phone" type="text" value="<?php echo isset($phone) ? $phone : ''; ?>"/></td></tr>
     <tr>
       <td><label for="job">Desired Job:</label></td>
-      <td><input id="job" name="job" type="text" value="<?php echo $job; ?>"/></td>
+      <td><input id="job" name="job" type="text" value="<?php echo isset($job) ? $job : ''; ?>"/></td>
   </tr>
   </table>
   <p>
     <label for="resume">Paste your resume here:</label><br />
-    <textarea id="resume" name="resume" rows="4" cols="40"><?php echo $resume; ?></textarea><br />
+    <textarea id="resume" name="resume" rows="4" cols="40"><?php echo isset($resume) ? $resume : ''; ?></textarea><br />
     <input type="submit" name="submit" value="Submit" />
   </p>
 </form>
